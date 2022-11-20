cf18eb0da24cf0b9dcadad5a2aa02020ee80c08d
Jamie Taylor <Jamie.Taylor@pobox.com>
2022-11-20 18:25:58 -0500

    fix GMCM deprecation warning

--- ToDew/ModEntry.cs
+++ ToDew/ModEntry.cs
@@ -311,7 +311,7 @@ Other flags:
         void AddSectionTitle(IManifest mod, Func<string> text, Func<string> tooltip = null);
         void AddBoolOption(IManifest mod, Func<bool> getValue, Action<bool> setValue, Func<string> name, Func<string> tooltip = null, string fieldId = null);
         void AddKeybind(IManifest mod, Func<SButton> getValue, Action<SButton> setValue, Func<string> name, Func<string> tooltip = null, string fieldId = null);
-        void AddNumberOption(IManifest mod, Func<int> getValue, Action<int> setValue, Func<string> name, Func<string> tooltip = null, int? min = null, int? max = null, int? interval = null, string fieldId = null);
+        void AddNumberOption(IManifest mod, Func<int> getValue, Action<int> setValue, Func<string> name, Func<string> tooltip = null, int? min = null, int? max = null, int? interval = null, Func<int, string> formatValue = null, string fieldId = null);
     }
     // See https://github.com/jltaylor-us/StardewGMCMOptions/blob/default/StardewGMCMOptions/IGMCMOptionsAPI.cs
     public interface GMCMOptionsAPI {
